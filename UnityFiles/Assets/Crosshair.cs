9952dc92ed8f2469655350f20a14a26b2e19048a
Alexis La Torre <alexis-latorre@live.fr>
2017-11-06 00:31:41 +0100

    Ajoute le fond et le curseur à la scène de tests

---
+++ UnityFiles/Assets/Crosshair.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 p = new Vector3();
+        Camera c = Camera.main;
+        Vector2 mousePos = new Vector2();
+        mousePos.x = Input.mousePosition.x;
+        mousePos.y = Input.mousePosition.y;
+        p = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, c.nearClipPlane + 9.7f));
+
+        this.transform.position = p;
+    }
+}
