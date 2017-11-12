4355496a59823244786bdd50b53569bbb6f96024
Alexis La Torre <alexis-latorre@live.fr>
2017-11-12 20:49:18 +0100

    Retravail du curseur

---
+++ UnityFiles/Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed = 6f;
+
+    Vector3 movement;
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = Input.GetAxisRaw("Horizontal");
+
+        Move(x);
+	}
+
+    void Move(float x)
+    {
+        movement.Set(x, 0f, 0f);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rigidBody.MovePosition(transform.position + movement);
+    }
+}
---
+++ UnityFiles/Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+    public float range = 100f;
+    public float delayBetweenShots = 2000f;
+    public float effectsDuration = 0.2f;
+
+    GameObject crosshair;
+    Ray shootRay;
+    RaycastHit shootHit;
+    float timer;
+    Light gunLight;
+    LineRenderer gunLine;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timer = 0f;
+        crosshair = GameObject.FindGameObjectWithTag("Crosshair");
+        gunLight = GetComponent<Light>();
+        gunLine = GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer >= delayBetweenShots && Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+
+        if (timer >= delayBetweenShots * effectsDuration)
+        {
+            DisableEffects();
+        }
+    }
+
+    void Shoot()
+    {
+        timer = 0f;
+        gunLight.enabled = true;
+        gunLine.enabled = true;
+
+        gunLine.SetPosition(0, this.transform.position);
+        gunLine.SetPosition(1, crosshair.transform.position);// * range);
+    }
+
+    void DisableEffects()
+    {
+        gunLight.enabled = false;
+        gunLine.enabled = false;
+    }
+}
