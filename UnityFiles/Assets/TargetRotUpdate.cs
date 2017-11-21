5bb6a18511a7d6a6d877da27f3577869674f573a
Marc Arancio <megama_1@hotmail.fr>
2017-11-21 21:13:03 +0100

    Le gun pointe dans la bonne direction (aka vers la cible :D)

--- UnityFiles/Assets/TargetRotUpdate.cs
+++ UnityFiles/Assets/TargetRotUpdate.cs
@@ -6,27 +6,35 @@ using UnityStandardAssets.Characters.ThirdPerson;
 public class TargetRotUpdate : MonoBehaviour {
 	public ThirdPersonCharacter player;
 	public GameObject cross;
+	Vector3 frontGun = new Vector3(10f,110f,-75f);
 
 	// Use this for initialization
 	void Start () {
-		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		Vector3 ang = frontGun;
 		Vector3 o = player.transform.position;
 		o.z = 0f;
 		o.y += 0.8f;
 		Vector3 mousePos = new Vector3();
-		mousePos.x = Input.mousePosition.x - o.x;
-		mousePos.y = Input.mousePosition.y - o.y;
-		mousePos = mousePos.normalized;
-		//Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0f);
-		Vector3 front = new Vector3(1,0,0);
-		Quaternion temp = new Quaternion();
-		temp.SetFromToRotation (front, mousePos);
-		//temp.SetLookRotation(mousePos);
+		mousePos.x = cross.transform.position.x - o.x;
+		mousePos.y = cross.transform.position.y - o.y;
+		float a = Mathf.Rad2Deg * Mathf.Atan(mousePos.y / mousePos.x);
+		/*if (a < 0) {
+			a += 180;
+		}*/
+
+		if (cross.transform.position.x < o.x) {
+			ang.y += 180;
+			ang.x = frontGun.x + a;
+		} else {
+			ang.x = frontGun.x - a;
+		}
+
+		this.transform.rotation = Quaternion.Euler(ang);
+		//this.transform.rotation = Quaternion.AngleAxis(90f, Vector3.right);
 		this.transform.position = cross.transform.position;
-		this.transform.rotation = temp;
 	}
 }
